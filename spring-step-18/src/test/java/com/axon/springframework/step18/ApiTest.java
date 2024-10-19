259003ff70f15a338d4fbde7bbfe9aceb8a4eaa8
rainbow-lpf <rainbow-lpf@outlook.com>
2024-10-19 19:39:51 +0800

    加入对spring整合jdbc配置的理解

--- spring-step-18/src/test/java/com/axon/springframework/step18/ApiTest.java
+++ spring-step-18/src/test/java/com/axon/springframework/step18/ApiTest.java
@@ -8,6 +8,58 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.Map;
 
+/**
+ *  本章主要整合了Jdbc的相关功能
+ *  核心底代码：
+ *  1.1>添加JdbcTemplate 继承了JdbcAccess， JdbcAccess中有dataSource的注入
+ *      public class JdbcTemplate extends JdbcAccess implements JdbcOperations
+ *      }
+ *
+ *      //TODO  JdbcAccess implements InitializingBean 实现了初始化的代码
+ *      public abstract class JdbcAccess implements InitializingBean {
+ *
+ *
+     *     private DataSource dataSource;
+     *
+     *     public DataSource getDataSource() {
+     *         return dataSource;
+     *     }
+     *
+     *     public void setDataSource(DataSource dataSource) {
+     *         this.dataSource = dataSource;
+     *     }
+     *
+     *     protected DataSource obtainDataSource() {
+     *         DataSource dataSource = getDataSource();
+     *         Assert.state(dataSource != null, "No DataSource set");
+     *         return dataSource;
+     *     }
+     *
+ *         //TODO 核心代码，初始化数据源
+     *     @Override
+     *     public void afterPropertiesSet() {
+     *         //TODO  通过注入的方式
+ *          if (getDataSource() == null) {
+     *             throw new RuntimeException("数据库配置未初始化");
+     *         }
+     *     }
+ *
+ * }
+ *
+ * 1.2> 加入相关的xml文件配置，通过xml解析，注入相关的配置
+ *       <bean id="dataSource" class="com.alibaba.druid.pool.DruidDataSource">
+ *         <property name="driverClass" value="com.mysql.cj.jdbc.Driver"/>
+ *         <property name="jdbcUrl" value="jdbc:mysql://192.168.2.104:3306/my_test_db?useSSL=false"></property>
+ *         <property name="username" value="root"></property>
+ *         <property name="password" value="123456"></property>
+ *     </bean>
+ *
+ *     <bean id="jdbcTemplate"
+ *           class="JdbcTemplate">
+ *         <property name="dataSource" ref="dataSource"/>
+ *     </bean>
+ *
+ */
 public class ApiTest {
 
 
